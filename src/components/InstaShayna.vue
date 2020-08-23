14847db8da381301fb335d6819bcaca27a70da22
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2020-08-23 14:45:33 +0700

    Upload Project Vue

---
+++ src/components/InstaShayna.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="instagram-photo">
+        <div class="insta-item set-bg" style="background-image: url('/img/insta-1.jpg')">
+            <div class="inside-text">
+                <i class="ti-instagram"></i>
+                <h5><a href="#">shayna_gallery</a></h5>
+            </div>
+        </div>
+        <div class="insta-item set-bg" data-setbg="img/insta-2.jpg" style="background-image: url('/img/insta-2.jpg')">
+            <div class="inside-text">
+                <i class="ti-instagram"></i>
+                <h5><a href="#">shayna_gallery</a></h5>
+            </div>
+        </div>
+        <div class="insta-item set-bg" data-setbg="img/insta-3.jpg" style="background-image: url('/img/insta-3.jpg')">
+            <div class="inside-text">
+                <i class="ti-instagram"></i>
+                <h5><a href="#">shayna_gallery</a></h5>
+            </div>
+        </div>
+        <div class="insta-item set-bg" data-setbg="img/insta-4.jpg" style="background-image: url('/img/insta-4.jpg')">
+            <div class="inside-text">
+                <i class="ti-instagram"></i>
+                <h5><a href="#">shayna_gallery</a></h5>
+            </div>
+        </div>
+        <div class="insta-item set-bg" data-setbg="img/insta-5.jpg" style="background-image: url('/img/insta-5.jpg')">
+            <div class="inside-text">
+                <i class="ti-instagram"></i>
+                <h5><a href="#">shayna_gallery</a></h5>
+            </div>
+        </div>
+        <div class="insta-item set-bg" data-setbg="img/insta-6.jpg" style="background-image: url('/img/insta-6.jpg')">
+            <div class="inside-text">
+                <i class="ti-instagram"></i>
+                <h5><a href="#">shayna_gallery</a></h5>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InstaShayna'
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <CarouselShayna />
+    <Carousel2Shayna />
+    <InstaShayna />
+    <IklanShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import CarouselShayna from '@/components/CarouselShayna.vue'
+import Carousel2Shayna from '@/components/Carousel2Shayna.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import IklanShayna from '@/components/IklanShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    CarouselShayna,
+    Carousel2Shayna,
+    InstaShayna,
+    IklanShayna,
+    FooterShayna
+  }
+}
+</script>
